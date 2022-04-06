bad62b190147a7a1b06708deddc4c935802b3816
Francisco Gallardo <f.gallardoa3108@gmail.com>
2022-04-05 21:05:29 -0400

    Se agregan pantallas de formulario de producto, y pantalla de code reader se agrega interaccion entre pantallas

--- app/src/main/java/com/example/controldestock/Menu.java
+++ app/src/main/java/com/example/controldestock/Menu.java
@@ -6,18 +6,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_menu);
     }
 
-    public void login(View vista){
-        Intent i = new Intent(this,Menu.class);
+    public void ingresarProducto(View vista){
+        Intent i = new Intent(this,Formulario.class);
         startActivity(i);
     }
-
-
+    public void leerCodigo(View vista){
+        Intent i = new Intent(this,leerCodigo.class);
+        startActivity(i);
+    }
 }
